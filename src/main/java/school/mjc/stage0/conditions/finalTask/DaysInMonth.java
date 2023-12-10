50964269692865f30b67d30a9502b87020af9230
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-10 13:47:03 +0500

    finished

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
+    private  String checkYear(int year){
+        String s=(year % 4 == 0 && year % 100!= 0 || year % 400 == 0)?"29":"28";
+        s=year<=0?"invalid date":s;
+        return s;
+    }
     public void printDays(int year, int month) {
-
+        String days = switch (month) {
+            case 1,3,5,7,8,10,12->"31";
+            case 4,6,9,11->"30";
+            case 2->checkYear(year);
+            default -> "invalid date";
+        };
+        System.out.println(days);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        String result;
+        if (divider==0) {
+            result="division by zero";
+        }else{
+            int a= dividend/divider;// 25/15=1
+            boolean check= (dividend-a*dividend)!=0;//25-15!=0?false:true
+            result = (check) ? "can be divided completely" : "cannot be divided completely";
+        }
+       
+        System.out.println(result);
     }
 }
